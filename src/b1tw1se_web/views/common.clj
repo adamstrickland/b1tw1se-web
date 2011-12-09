e97e1f2b6995267a9ee45c38136f81c1d210c43d
Adam Strickland <adam.strickland@gmail.com>
2011-12-09 16:33:19 -0600

    upd common layout

--- src/b1tw1se_web/views/common.clj
+++ src/b1tw1se_web/views/common.clj
@@ -12,12 +12,25 @@
                 [:meta {:name "author",  :content ""}]
                 [:meta {:name "viewport", :content "width=device-width,initial-scale=1"}]
                 ; (include-css "/css/reset.css")
-                (include-css "/css/style.css")
-                (include-js "/js/libs/modernizr-2.0.6.min.js")
+                (include-css "/css/style.css" "/css/noir.css")
+                ; (include-js "/js/libs/modernizr-2.0.6.min.js")
               ]
               [:body
-                [:div#wrapper
-                  content
+                [:div#container
+                  [:header ""]
+                  [:div#main {:role "main"} content]
+                  [:footer ""]
                 ]
+                ; [:script {:src "//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"}]
+                ; [:script "window.jQuery || document.write('<script src=''js/libs/jquery-1.6.2.min.js''></script>')"]
+                ; [:script {:defer "", :src "js/plugins.js"}]
+                ; [:script {:defer "", :src "js/script.js"}]
+                ; (javascript-tag
+                ;                 `("window.gaq = [['_setAccount', 'UAXXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];" 
+                ;                  "Modernizr.load({"
+                ;                  "  load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'"
+                ;                  "});"
+                ;                 )
+                ;               )
               ]
             ))
