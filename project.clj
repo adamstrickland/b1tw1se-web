248b3dff4853b8302f6ca5bfde389a32d8daf5b0
Adam Strickland <adam.strickland@gmail.com>
2011-12-19 17:01:21 -0600

    upd mongo example

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject b1tw1se-web "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.1"]
+                           [congomongo "0.1.5-SNAPSHOT"]]
             :main b1tw1se-web.server)
 
