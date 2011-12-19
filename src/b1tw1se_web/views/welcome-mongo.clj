7767cd8a8a529774e3e73923db3a0003f58e109f
Adam Strickland <adam.strickland@gmail.com>
2011-12-19 16:52:14 -0600

    added mongo example

--- src/b1tw1se_web/views/welcome-mongo.clj
+++ src/b1tw1se_web/views/welcome-mongo.clj
@@ -1,5 +1,5 @@
-(ns noir-mongo-heroku.views.welcome
-  (:require [noir-mongo-heroku.views.common :as common]
+(ns b1tw1se-web.views.welcome-mongo
+  (:require [b1tw1se-web.views.common :as common]
             [noir.content.pages :as pages])
   (use noir.core
        hiccup.core
